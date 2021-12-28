f42e198caf9daee761541c83c775d43a3caccb73
frankmendez <frankmendezresources@gmail.com>
2021-12-28 18:08:04 +0800

    Added income and expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container, Header, Statistic } from 'semantic-ui-react';
+import { Container, Grid, Header, Segment, Statistic } from 'semantic-ui-react';
 
 function App() {
 	return (
@@ -8,6 +8,30 @@ function App() {
 				<Statistic.Label>Your Balance:</Statistic.Label>
 				<Statistic.Value>2500.53</Statistic.Value>
 			</Statistic>
+			<Segment textAlign='center'>
+				<Grid columns={2} divided>
+					<Grid.Row>
+						<Grid.Column>
+							<Statistic size='tiny' color='green'>
+								<Statistic.Label sytle={{ textAlign: 'left' }}>
+									{' '}
+									Incoming:
+								</Statistic.Label>
+								<Statistic.Value>1034.50</Statistic.Value>
+							</Statistic>
+						</Grid.Column>
+						<Grid.Column>
+							<Statistic size='tiny' color='red'>
+								<Statistic.Label sytle={{ textAlign: 'left' }}>
+									{' '}
+									Expenses:
+								</Statistic.Label>
+								<Statistic.Value>134.50</Statistic.Value>
+							</Statistic>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
 		</Container>
 	);
 }
